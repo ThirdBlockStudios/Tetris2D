f9f143c0a868acc95fff2c3e8f168d55a1bdd5ad
Ryan <ryanpun282@gmail.com>
2023-07-14 19:30:13 -0700

    sprites, center of rotation

--- main.gd
+++ main.gd
@@ -6,33 +6,37 @@ extends Node
 
 var initial_piece
 
+func _init():    
+    print("main 0")
+
 ## Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize() ## randomize rng seed
-	initial_piece = piece_scene.instantiate()
-	initial_piece.setType(randi() % 7) ## randomize piece type
-	initial_piece.position = Vector2(112, 32) ## place block at "center" top
-	add_child(initial_piece)
-	pass  # Replace with function body.
+    print("main 1")
+    randomize() ## randomize rng seed
+    initial_piece = piece_scene.instantiate()
+    initial_piece.setType(randi() % 7) ## randomize piece type
+    initial_piece.position = Vector2(144, 0) ## place block at "center" top
+    add_child(initial_piece)
+    pass  # Replace with function body.
 
 ## Called when an InputEventKey hasn't been consumed by _input or any GUI Control item (i.e., gameplay input).
 func _unhandled_key_input(event: InputEvent):
-	if event.is_action_pressed("spawn_piece_testing"):
-		spawn_new()
-	pass
+    if event.is_action_pressed("spawn_piece_testing"):
+        spawn_new()
+    pass
 
 ## Test function for spawning a new piece
 func spawn_new():
-	initial_piece.queue_free()
-	_ready()
+    initial_piece.queue_free()
+    _ready()
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 ## This is the game's main loop called every game tick. It is primarily responsible for updating the active piece's position (falling).
 ## The tick rate or speed of the game is determined by the current wait time of TickTimer.
 func _on_tick():
-	# TODO: speed up game -> decrease TickTimer wait time by percentage based on either points or
-	pass  # Replace with function body.
+    # TODO: speed up game -> decrease TickTimer wait time by percentage based on either points or
+    pass  # Replace with function body.
