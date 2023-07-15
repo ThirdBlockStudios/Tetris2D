2e04500a33deedf988836799b13d7d2a130ba652
nkuang <nkuang123@gmail.com>
2023-07-15 09:23:49 -0700

    first-pass Board logic

--- tile_map.gd
+++ tile_map.gd
@@ -1,36 +1,84 @@
 class_name Board
 extends TileMap
 
-var x_pos = 0
-var y_pos = 0
-var er = false
+# Local variables.
+var dimensions = Vector2i(10, 20)
+var board = []
+
 
 ## Called when the node enters the scene tree for the first time.
 func _ready():
-    x_pos = 0
-    y_pos = 0
-    set_cell(0, Vector2i(x_pos, y_pos), 0, Vector2i(0,0), 0)
+	for x in range(dimensions.x):
+		board.append(Array())
+		for y in range(dimensions.y):
+			board[x].append(0)
+	var used_cells = get_used_cells(0)
+	for cell in used_cells:
+		board[cell.x][cell.y] = true
 
-## Updates the board.
-func Board_update():
-    pass
 
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    # Process 10 tiles at a time. Looping draw/erase.
-    for i in range(10):
-        y_pos = (y_pos + 1)
-        
-        if (y_pos > 40):
-            y_pos = 0
-            x_pos += 1
-            
-        if (x_pos > 30):
-            x_pos = 0
-            y_pos = 0
-            er = not er
+## Checks if the given row is full.
+func Board_isRowFull(row: int) -> bool:
+	var is_full = true
+	for cols in dimensions.x:
+		is_full = is_full and board[cols][row]
+	return is_full
 
-        if (er):
-            erase_cell(0, Vector2i(x_pos, y_pos))
-        else:
-            set_cell(0, Vector2i(x_pos, y_pos), 0, Vector2i(0,0), 0)
+
+## Clears the given row.
+func Board_clearRow(row: int):
+	for cols in dimensions.x:
+		board[cols][row] = false
+		erase_cell(0, Vector2i(cols, row))
+
+
+## Recursively updates the board given a starting row. A step size can be
+## optionally provided.
+## TODO(nkuang): default step to 1 as an optional parameter.
+func Board_updateRow(row: int, step: int):
+	if row < 0:
+		return
+	else:
+		for cols in dimensions.x:
+			if board[cols][row]:
+				board[cols][row] = false
+				var new_row = row + step
+				board[cols][new_row] = true
+				set_cell(
+					0,
+					Vector2i(cols, new_row),
+					get_cell_source_id(0, Vector2i(cols, row)),
+					Vector2i(0, 0),
+					0
+				)
+				erase_cell(0, Vector2i(cols, row))
+		Board_updateRow(row - 1, step)
+
+
+## Piece processing logic when we need to update the board state.
+func Board_playTiles():
+	# Testing clearing by hardcoding a 5-high I block with base at (8, 19).
+	var hardcode_block_x = 8
+	var hardcode_block_y = 19
+
+	# Rendering.
+	set_cell(0, Vector2i(hardcode_block_x, hardcode_block_y), 0, Vector2i(0, 0), 0)
+	set_cell(0, Vector2i(hardcode_block_x, hardcode_block_y - 1), 0, Vector2i(0, 0), 0)
+	set_cell(0, Vector2i(hardcode_block_x, hardcode_block_y - 2), 0, Vector2i(0, 0), 0)
+	set_cell(0, Vector2i(hardcode_block_x, hardcode_block_y - 3), 0, Vector2i(0, 0), 0)
+	set_cell(0, Vector2i(hardcode_block_x, hardcode_block_y - 4), 0, Vector2i(0, 0), 0)
+	board[hardcode_block_x][hardcode_block_y] = true
+	board[hardcode_block_x][hardcode_block_y - 1] = true
+	board[hardcode_block_x][hardcode_block_y - 2] = true
+	board[hardcode_block_x][hardcode_block_y - 3] = true
+	board[hardcode_block_x][hardcode_block_y - 4] = true
+
+	var rows_processed = 0
+
+	# Processing from the bottom to the top.
+	while rows_processed < hardcode_block_y:
+		if Board_isRowFull(hardcode_block_y - rows_processed):
+			Board_clearRow(hardcode_block_y - rows_processed)
+			Board_updateRow(hardcode_block_y - rows_processed, 1)
+		else:
+			rows_processed += 1
