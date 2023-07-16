d42bad2bed1468b727eaddafe3da456d434649c5
Norman <nkuang@vassar.edu>
2023-07-15 17:19:51 -0700

    add comments

--- tile_map.gd
+++ tile_map.gd
@@ -2,7 +2,11 @@ class_name Board
 extends TileMap
 
 # Local variables.
+
+# Game dimensions.
 var dimensions = Vector2i(10, 20)
+
+# 2D game board to store piece positions and state.
 var board = []
 
 
@@ -12,6 +16,7 @@ func _ready():
 		board.append(Array())
 		for y in range(dimensions.y):
 			board[x].append(0)
+	# Mark all currently used cells in the board with their coordinates.
 	var used_cells = get_used_cells(0)
 	for cell in used_cells:
 		board[cell.x][cell.y] = true
@@ -36,14 +41,17 @@ func Board_clearRow(row: int):
 ## optionally provided.
 ## TODO(nkuang): default step to 1 as an optional parameter.
 func Board_updateRow(row: int, step: int):
+	# Recursion base case.
 	if row < 0:
 		return
 	else:
 		for cols in dimensions.x:
+			# We only need to update a piece if it exists.
 			if board[cols][row]:
 				board[cols][row] = false
 				var new_row = row + step
 				board[cols][new_row] = true
+				# Update by rendering the new position and erasing the old.
 				set_cell(
 					0,
 					Vector2i(cols, new_row),
@@ -52,6 +60,7 @@ func Board_updateRow(row: int, step: int):
 					0
 				)
 				erase_cell(0, Vector2i(cols, row))
+		# Process the rows above.
 		Board_updateRow(row - 1, step)
 
 
