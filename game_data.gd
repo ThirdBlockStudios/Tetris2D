f09dbf51d0ab2aff48cdfedb4180b23a9e01c052
Ryan <ryanpun282@gmail.com>
2023-07-19 22:15:45 -0700

    fixed rotation center offset values for line and square, added rotation validation

--- game_data.gd
+++ game_data.gd
@@ -8,31 +8,31 @@ const sine = sin(PI / 2)  ## = 1
 const rotation_matrix = [cosine, sine, -sine, cosine]
 
 const tile_ids = {
-	TETROMINO.square: 0,
-	TETROMINO.tee: 1,
-	TETROMINO.l_left: 2,
-	TETROMINO.l_right: 3,
-	TETROMINO.zag_right: 4,
-	TETROMINO.zag_left: 5,
-	TETROMINO.line: 6
+    TETROMINO.square: 0,
+    TETROMINO.tee: 1,
+    TETROMINO.l_left: 2,
+    TETROMINO.l_right: 3,
+    TETROMINO.zag_right: 4,
+    TETROMINO.zag_left: 5,
+    TETROMINO.line: 6
 }
 
 const blocks = {
-	TETROMINO.square: [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
-	TETROMINO.tee: [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, 0)],
-	TETROMINO.l_left: [Vector2(-1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	TETROMINO.l_right: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(1, -1)],
-	TETROMINO.zag_right: [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, -1)],
-	TETROMINO.zag_left: [Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0)],
-	TETROMINO.line: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)]
+    TETROMINO.square: [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
+    TETROMINO.tee: [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, 0)],
+    TETROMINO.l_left: [Vector2(-1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+    TETROMINO.l_right: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(1, -1)],
+    TETROMINO.zag_right: [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, -1)],
+    TETROMINO.zag_left: [Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0)],
+    TETROMINO.line: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)]
 }
 
 const center_offset = {
-	TETROMINO.square: Vector2(0.5, 0.5),
-	TETROMINO.tee: Vector2(0, 0),
-	TETROMINO.l_left: Vector2(0, 0),
-	TETROMINO.l_right: Vector2(0, 0),
-	TETROMINO.zag_right: Vector2(0, 0),
-	TETROMINO.zag_left: Vector2(0, 0),
-	TETROMINO.line: Vector2(0.5, -0.5)
+    TETROMINO.square: Vector2(-0.5, -0.5),
+    TETROMINO.tee: Vector2(0, 0),
+    TETROMINO.l_left: Vector2(0, 0),
+    TETROMINO.l_right: Vector2(0, 0),
+    TETROMINO.zag_right: Vector2(0, 0),
+    TETROMINO.zag_left: Vector2(0, 0),
+    TETROMINO.line: Vector2(-0.5, -0.5)
 }
