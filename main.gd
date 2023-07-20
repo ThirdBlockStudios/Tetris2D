4459a93ee0fea83dad7d8396300cb4330c68c5d4
rp282 <ryanpun282@gmail.com>
2023-07-20 10:22:14 -0700

    drew sprites

--- main.gd
+++ main.gd
@@ -15,41 +15,41 @@ var next_queue: Array[int] = []
 var spawn_position = Vector2(4, 0)
 
 var tetrominoes = {
-    GameData.TETROMINO.square: {},
-    GameData.TETROMINO.tee: {},
-    GameData.TETROMINO.l_left: {},
-    GameData.TETROMINO.l_right: {},
-    GameData.TETROMINO.zag_right: {},
-    GameData.TETROMINO.zag_left: {},
-    GameData.TETROMINO.line: {}
+	GameData.TETROMINO.square: {},
+	GameData.TETROMINO.tee: {},
+	GameData.TETROMINO.l_left: {},
+	GameData.TETROMINO.l_right: {},
+	GameData.TETROMINO.zag_right: {},
+	GameData.TETROMINO.zag_left: {},
+	GameData.TETROMINO.line: {}
 }
 
 
 ## Start game. Called when the node enters the scene tree for the first time.
 func _ready():
-    print("cosine:", GameData.cosine)
-    print("sine:", GameData.sine)
-    board = get_tree().get_first_node_in_group("board")
-    piece = get_tree().get_first_node_in_group("piece")
+	print("cosine:", GameData.cosine)
+	print("sine:", GameData.sine)
+	board = get_tree().get_first_node_in_group("board")
+	piece = get_tree().get_first_node_in_group("piece")
 
-    randomize()  # randomize rng seed
+	randomize()  # randomize rng seed
 
-    # Set game constants (cell arrangements, tile type map)
-    for i in GameData.TETROMINO:
-        tetrominoes[GameData.TETROMINO[i]] = {
-            "type": GameData.TETROMINO[i],
-            "blocks": GameData.blocks[GameData.TETROMINO[i]],
-            "tile_id": GameData.tile_ids[GameData.TETROMINO[i]],
-            "center_offset": GameData.center_offset[GameData.TETROMINO[i]]
-        }
-    board.reset_board()
-    spawn_piece()
+	# Set game constants (cell arrangements, tile type map)
+	for i in GameData.TETROMINO:
+		tetrominoes[GameData.TETROMINO[i]] = {
+			"type": GameData.TETROMINO[i],
+			"blocks": GameData.blocks[GameData.TETROMINO[i]],
+			"tile_id": GameData.tile_ids[GameData.TETROMINO[i]],
+			"center_offset": GameData.center_offset[GameData.TETROMINO[i]]
+		}
+	board.reset_board()
+	spawn_piece()
 
 
 func game_over():
-    $GameOverSound.play()
-    board.reset_board()
-    spawn_piece()
+	$GameOverSound.play()
+	board.reset_board()
+	spawn_piece()
 
 
 #    held_piece = null
@@ -57,34 +57,34 @@ func game_over():
 
 
 func hold_piece(piece: Piece):
-    # TODO
-    pass
+	# TODO
+	pass
 
 
 func spawn_piece():
-    if next_queue.size() < queue_size:
-        for i in range(queue_size - next_queue.size()):
-            var random_int = randi() % tetrominoes.size()
+	if next_queue.size() < queue_size:
+		for i in range(queue_size - next_queue.size()):
+			var random_int = randi() % tetrominoes.size()
 #            var random_int = 6
-            next_queue.append(random_int)
-    var current_piece = next_queue.pop_front()
-    var data = tetrominoes[current_piece]
-    next_queue_board.initialize(next_queue)  # TODO
-    piece.initialize(spawn_position, data)
+			next_queue.append(random_int)
+	var current_piece = next_queue.pop_front()
+	var data = tetrominoes[current_piece]
+	next_queue_board.initialize(next_queue)  # TODO
+	piece.initialize(spawn_position, data)
 #    print(piece.blocks.map(func (b): b.position + Vector2(4, 0)))
-    if board.Board_isMoveValid(piece, spawn_position):
-        board.Board_setPiece(piece)
-    else:
-        game_over()
+	if board.Board_isMoveValid(piece, spawn_position):
+		board.Board_setPiece(piece)
+	else:
+		game_over()
 
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 
 ## This is the game's main loop called every game tick. It is primarily responsible for updating the active piece's position (falling).
 ## The tick rate or speed of the game is determined by the current wait time of TickTimer.
 func _on_tick():
-    # TODO: speed up game -> decrease TickTimer wait time by percentage based on either points or
-    pass  # Replace with function body.
+	# TODO: speed up game -> decrease TickTimer wait time by percentage based on either points or
+	pass  # Replace with function body.
