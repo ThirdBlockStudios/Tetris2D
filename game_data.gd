dff1af31d0b6b0d74b9d08e420e87388fd579f28
Ryan <ryanpun282@gmail.com>
2023-07-23 18:04:36 -0700

    rewrote game data class

--- game_data.gd
+++ game_data.gd
@@ -1,41 +1,54 @@
 class_name GameData
-extends Node
+extends Object
 
-enum TETROMINO { square, tee, l_left, l_right, zag_right, zag_left, line } # , double_square }
 
 const cosine = cos(PI / 2)  ## = 0
 const sine = sin(PI / 2)  ## = 1
-const rotation_matrix = [cosine, sine, -sine, cosine]
+const rotation_matrix = [cosine, sine, -sine, cosine] ## Rotation constants for 90 degree clockwise rotation. Refer here for more information about the math https://www.cuemath.com/algebra/rotation-matrix/.
 
-const tile_ids = {
-    TETROMINO.square: 0,
-    TETROMINO.tee: 1,
-    TETROMINO.l_left: 2,
-    TETROMINO.l_right: 3,
-    TETROMINO.zag_right: 4,
-    TETROMINO.zag_left: 5,
-    TETROMINO.line: 6, 
-#    TETROMINO.double_square: 0
-}
 
-const blocks = {
-    TETROMINO.square: [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
-    TETROMINO.tee: [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, 0)],
-    TETROMINO.l_left: [Vector2(-1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-    TETROMINO.l_right: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(1, -1)],
-    TETROMINO.zag_right: [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, -1)],
-    TETROMINO.zag_left: [Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0)],
-    TETROMINO.line: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
-#    TETROMINO.double_square: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(-1, 1), Vector2(0, 1), Vector2(1, 1), Vector2(2, 1)]
-}
+## To add new tetrominos add your tetromino name here then add an 
+## entry to tetrominos below (see @/models/tetromino.gd for required parameters).
+enum { square, tee, l_left, l_right, zag_right, zag_left, line }
 
-const center_offset = {
-    TETROMINO.square: Vector2(-0.5, -0.5),
-    TETROMINO.tee: Vector2(0, 0),
-    TETROMINO.l_left: Vector2(0, 0),
-    TETROMINO.l_right: Vector2(0, 0),
-    TETROMINO.zag_right: Vector2(0, 0),
-    TETROMINO.zag_left: Vector2(0, 0),
-    TETROMINO.line: Vector2(-0.5, -0.5),
-#    TETROMINO.double_square: Vector2(-0.5, -0.5)
+
+const ghost_layer = 2
+const ghost_block_tile_id = 7
+const background_layer = 1
+const background_tile_id = 8
+
+
+## Tetrominos available to spawn in game.
+var tetrominoes = {
+    square: Tetromino.new(
+        square, # Tile ID
+        [Vector2(0, 0), Vector2(0, 1), Vector2(1, 0), Vector2(1, 1)], # Block grid coordinates
+        Vector2(-0.5, -0.5) # Rotation center offset
+    ),
+    tee: Tetromino.new(
+        tee,
+        [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, 0)]
+    ),
+    l_right: Tetromino.new(
+        l_right,
+        [Vector2(-1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)]
+    ),
+    l_left: Tetromino.new(
+        l_left,
+        [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(1, -1)]
+    ),
+    zag_right: Tetromino.new(
+        zag_right,
+        [Vector2(-1, 0), Vector2(0, 0), Vector2(0, -1), Vector2(1, -1)]
+    ),
+    zag_left: Tetromino.new(
+        zag_left,
+        [Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0)]
+    ),
+    line: Tetromino.new(
+        line,
+        [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
+        Vector2(-0.5, -0.5)
+    ),
 }
+    
---
+++ models/tetromino.gd
@@ -0,0 +1,14 @@
+## Base tetromino class
+class_name Tetromino
+
+
+var tile_id: int ## Id of the tile map sprite used to draw tetromino. Defaults to 0 (yellow sprite).
+var block_grid_coordinates: Array[Vector2] = [] ## Tile coordinates (in tile map grid format) of the blocks that make up the tetromino.
+var rotation_center_offset: Vector2 ## Offset (from 0, 0?) around which the blocks of the piece rotate. Defaults to (0, 0). 
+
+
+## Constructor called on Tetromino.new()
+func _init(tile_id: int, block_grid_coordinates: Array[Vector2], rotation_center_offset: Vector2 = Vector2(0, 0)):
+    self.tile_id = tile_id
+    self.block_grid_coordinates = block_grid_coordinates
+    self.rotation_center_offset = rotation_center_offset    
