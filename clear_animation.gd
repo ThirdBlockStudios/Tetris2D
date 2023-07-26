0806d801d75c26d32f42eb4fc68c294e15dd12cc
Norman Kuang <nkuang123@gmail.com>
2023-07-25 22:32:41 -0700

    add fading animation

---
+++ clear_animation.gd
@@ -0,0 +1,24 @@
+class_name Explosion
+extends Node2D
+
+# Local variables.
+@onready var animated_sprite = $AnimatedSprite2D
+
+
+## Signal emitted by AnimationSprite2D.animation_finished(). Without this, the
+## animation will loop.
+func Explosion_stop():
+    animated_sprite.stop()
+    queue_free()
+
+
+## Moves the current animation position to coordinates (px).
+func Explosion_updateCoordinates(coordinates: Vector2i):
+    self.position.x = coordinates.x
+    self.position.y = coordinates.y
+
+
+## Starts the animation.
+func Explosion_start(delay_sec: float):
+    await get_tree().create_timer(delay_sec).timeout
+    animated_sprite.play()
